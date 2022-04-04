903e2f7db5a81d85515de58e21845f0297d717c5
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-04 08:16:46 +0800

    1.2: course info, step 2

--- course-info/src/App.js
+++ course-info/src/App.js
@@ -8,10 +8,19 @@ const Content = props => {
   console.log([props.part1, props.part2, props.part3, props.exercises1, props.exercises2, props.exercises3])
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
+  )
+}
+
+const Part = props => {
+  console.log([props.part, props.exercises])
+  return (
+  <>
+    <p>{props.part} {props.exercises}</p>
+  </>
   )
 }
 
